ac49f22e830a686e4048705ea80758334bb77da0
rvraagusan <ravi.ragul17@gmail.com>
2025-08-22 12:07:04 +0530

    working with state define

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Counter App"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Current Count:",
+            style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              "$_counter",
+              style: const TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _decrement,
+                  style: ElevatedButton.styleFrom(primary: Colors.red), 
+                  child: const Icon(Icons.remove)
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _reset,
+                  style: ElevatedButton.styleFrom(primary: Colors.grey),
+                  child: const Icon(Icons.refresh)
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _increment,
+                  style: ElevatedButton.styleFrom(primary: Colors.green), 
+                  child: const Icon(Icons.add),
+                  )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
